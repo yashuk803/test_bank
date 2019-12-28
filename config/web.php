59d8db57ee68402ba6d761151372463af074412e
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-12-28 20:27:48 +0200

    initial commit

---
+++ config/web.php
@@ -0,0 +1,69 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'container' => [
+        'singletons' => [
+            'app\repositories\EmployeeRepository' => ['class' => 'app\repositories\YiiEmployeeRepository'],
+            'app\repositories\contracts\ClientDepositRepository' => ['class' => 'app\repositories\EloquentClientDeposit'],
+            'app\repositories\contracts\HistoryTransactionRepository' => ['class' => 'app\repositories\EloquentHistoryTransaction'],
+            'EmployeeService' => ['class' => 'app\services\EmployeeService'],
+            'InterestAccrual' => ['class' => 'app\services\InterestAccrual'],
+            'ChargeCommission' => ['class' => 'app\services\ChargeCommission'],
+            'FactoryCommission' => ['class' => 'app\services\FactoryCommission'],
+        ],
+    ],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => 'lI2EDucBYy8aRmFzJU6ItN5N0d7CnLwV',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ controllers/SiteController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\controllers;
+
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+
+class SiteController extends Controller
+{
+
+    public function __construct(
+        $id,
+        $module,
+        $config = []
+    )
+    {
+        parent::__construct($id, $module, $config);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+
+        return $this->render('index');
+    }
+
+}
